829e1ad9805f4d5b548341f9fc050bac762382e9
mcthoren <thoren.mcdole@gmail.com>
2021-08-26 11:47:49 +0000

    comment out non-existent stuff for now. seive out diff sensors. correct filename.

--- 9dof_rx.go
+++ 9dof_rx.go
@@ -15,14 +15,18 @@ NDoF_DIR='/import/home/ghz/repos/lora_oled'
 # lock is also checked for and deleted on boot, in case of a crash
 touch "${LOCK}"
 
-"${WT_DIR}/grab_48h" "${DAT_DIR}" 9dof.dat
+"${WT_DIR}/grab_48h" "${DAT_DIR}" 9dof_raw.dat
+
+# seive out the 3 diff sensors. this opens up the possibility of stricter REs if necessary.
+grep "mx:" "${DAT_DIR}/9dof_raw.dat.2-3_day" > "${DAT_DIR}/mag.dat.2-3_day"
+grep "gx:" "${DAT_DIR}/9dof_raw.dat.2-3_day" > "${DAT_DIR}/gyro.dat.2-3_day"
+grep "ax:" "${DAT_DIR}/9dof_raw.dat.2-3_day" > "${DAT_DIR}/accel.dat.2-3_day"
 
 cd /home/ghz/9dof/plots || exit 1
-gnuplot "${NDoF_DIR}/linty.9dof.gnuplot"
+# gnuplot "${NDoF_DIR}/linty.9dof.gnuplot"
 
 sync
 
-/usr/bin/rsync -ur --timeout=50 /home/ghz/9dof/ "${NDoF_DIR}/"
-	wx_0x0a_sync:/wx_0x0a/ # 2> /dev/null
+# /usr/bin/rsync -ur --timeout=50 /home/ghz/9dof/ "${NDoF_DIR}/" wx_0x0a_sync:/wx_0x0a/ # 2> /dev/null
 
 rm "${LOCK}"
